b3b6d14087fe192e191e64730f6dc71d448f8717
MariamKiknavelidze <kiknavelidzem1@gmail.com>
2024-11-12 13:04:37 +0400

    End of 3th

---
+++ app.js
@@ -0,0 +1,27 @@
+// string
+let yourName = "Mariam";
+console.log(yourName);
+//number
+let a = 10;
+let b = 12;
+let c = a + b;
+console.log(c);
+
+//boolean
+let myName = "Mariam";
+let answer = Boolean(myName);
+
+console.log(answer);
+
+// undefine
+let producQuantity;
+var productName;
+
+// productPrice = 100;
+
+console.log("producQuantity", producQuantity);
+console.log("productName", productName);
+// 3.
+ let myName="mariam";
+ console.log("my name is:",myName)
+ let dayOfWeek=
